b4d2e129c1e6eddce13a2a8dd4657117bc87b16f
Florian Théron <florian.theron@epitech.eu>
2020-05-11 11:50:02 +0200

    Update CMakeLists (remove ZERO_CHECK from generation)

--- Sources/main.cpp
+++ Sources/main.cpp
@@ -1,6 +1,13 @@
 #include <Irrlicht/irrlicht.h>
 
+using namespace irr;
+
 int main()
 {
+    IrrlichtDevice *device = createDevice(video::EDT_OPENGL, core::dimension2d<u32>(1280, 720));
+
+	while (device->run());
+
+	device->drop();
 	return 0;
 }
