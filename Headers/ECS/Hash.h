459d6da51251b199a508a73aaf50d1e7a9805b96
Florian Theron <florian.theron@epitech.eu>
2020-06-12 17:05:14 +0200

    Update Hash.h

--- Headers/ECS/Hash.h
+++ Headers/ECS/Hash.h
@@ -12,3 +12,11 @@ constexpr std::uint32_t operator"" _hash(char const* s, std::size_t count)
     return fnv1a_32(s, count);
 }
 
+class Hash
+{
+public:
+    static std::uint32_t HashString(const std::string s, std::size_t count)
+    {
+        return ((count ? HashString(s, count - 1) : 2166136261u) ^ s[count]) * 16777619u;
+    }
+};
