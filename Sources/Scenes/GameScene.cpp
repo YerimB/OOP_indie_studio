126df08685f3321f1c211eaa655493b6e438f3ab
yarim1 <romain.bry@epitech.eu>
2020-06-13 11:24:23 +0200

    Map

--- Sources/Scenes/GameScene.cpp
+++ Sources/Scenes/GameScene.cpp
@@ -49,6 +49,7 @@ void GameScene::LoadAssets(GameManager* gm)
     auto sm = gm->GetSceneManager();
     this->AddMesh(sm->getMesh("Assets/sydney.md2"), "Sydney");
     this->AddMesh(sm->getMesh("Assets/wall.md3"), "Wall");
+    this->AddMesh(sm->getMesh("Assets/bomberman_m.obj"), "Bomber");
 }
 
 // Load Entities & Components
@@ -62,49 +63,13 @@ void GameScene::LoadElements(GameManager* gm)
         // Initialize component and set attributes then add it to entity
         if (b1->Initialize(nullptr)) {
             b1->SetButtonID(Button::ButtonID::QUIT);
-            b1->SetTexture(this->GetTexture("Sand"));
             b1->SetText("Back to menu");
             b1->SetPosition({ 50, 50 });
             b1->SetSize(300, 100);
-            b1->SetTextureToFit(true);
             b1->SetOnPress(changeSceneToMenu);
             e1.AddComponent(std::move(b1), Button::Id);
             // When done, add entity to the entity manager.
             gm->GetEntityManager()->AddEntity(e1);
-        }
-    }
-    {
-        Entity e2("AnimatedCharacter");
-        Drawable* d1 = new Drawable(gm->GetSceneManager());
-        Transform* t1 = new Transform();
-        Animator* a1 = new Animator(gm->GetSceneManager());
-        Collider* c1 = new Collider();
-
-        if (t1->Initialize(0) && d1->Initialize(this->GetMesh("Sydney")) && \
-            c1->Initialize() && a1->Initialize(d1->GetDrawable())) {
-            a1->AddAnimation("idle", { 0, 13, 15 });
-            a1->PlayAnimation("idle");
-            e2.AddComponent(d1, Drawable::Id);
-            e2.AddComponent(t1, Transform::Id);
-            e2.AddComponent(a1, Animator::Id);
-            e2.AddComponent(c1, Collider::Id);
-            gm->GetEntityManager()->AddEntity(e2);
-        }
-    }
-    {
-        Entity e3("Wall");
-        Drawable* d2 = new Drawable(gm->GetSceneManager());
-        Transform* t2 = new Transform();
-        Collider* c2 = new Collider();
-
-        if (d2->Initialize(this->GetMesh("Wall")) && \
-            t2->Initialize(nullptr) && c2->Initialize(nullptr))
-        {
-            t2->SetPosition({ 20, 0, 0 });
-            e3.AddComponent(d2, d2->Id);
-            e3.AddComponent(t2, t2->Id);
-            e3.AddComponent(c2, c2->Id);
-            gm->GetEntityManager()->AddEntity(e3);
         }
     }
 
@@ -120,9 +85,6 @@ void GameScene::Load(GameManager* gameManager)
     this->LoadSystems(gameManager);
     this->LoadAssets(gameManager);
     this->LoadElements(gameManager);
-
-    // Add Camera to Scene.
-    gameManager->GetSceneManager()->addCameraSceneNode(0, { 50, 100, 150 }, { 0, 100, -50 });
 }
 
 void GameScene::Update(GameManager* gameManager)
