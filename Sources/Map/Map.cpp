22cbfeb000c8cc4bcced8a6ded47ab701125f286
yarim1 <romain.bry@epitech.eu>
2020-06-15 18:47:13 +0200

    Win condition

--- Sources/Map/Map.cpp
+++ Sources/Map/Map.cpp
@@ -115,6 +115,7 @@ void Map::InitPlayers(const std::size_t& size, Scene *sc)
 				player.AddComponent(t0, Transform::Id);
 				player.AddComponent(cl0, Collider::Id);
 				player.AddComponent(a0, Animator::Id);
+				m_GameManager->m_globalVars.playersData[idx].alive = true;
 				m_GameManager->GetEntityManager()->AddEntity(player);
 			}
 		}
