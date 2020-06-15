efa730c1aeac40fbb9be27e1675e176fb486dac7
yarim1 <romain.bry@epitech.eu>
2020-06-15 17:56:32 +0200

    Revert "DON'T GET THIS VERSION"
    This reverts commit 27796cfddd69024a4d7577de8ae9ad25eacd5fbb, reversing
    changes made to ac537ee0421cbe3b43c0558b7b94bad5b917b064.

--- Headers/AI/AI.h
+++ Headers/AI/AI.h
@@ -7,9 +7,8 @@ class AI {
     public:
         AI(std::vector<std::string> map);
         std::array<int, 2> getDir();
-        bool isInBombRange();
     private:
-
+        bool isInBombRange();
         void getSafePath();
         void updateBombMap(std::vector<std::string> &map, std::array<int, 2> bpos);
         std::vector<std::string> _map;
