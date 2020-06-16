d40ce3b40b4229b5c22b232574c166e798c7e1a5
alexis <alexis.wirth@epitech.eu>
2020-06-16 02:00:11 +0200

    Feature: modify button'q position

--- CMakeFiles/bomberman.dir/cmake_clean.cmake
+++ CMakeFiles/bomberman.dir/cmake_clean.cmake
@@ -39,8 +39,8 @@ file(REMOVE_RECURSE
   "CMakeFiles/bomberman.dir/Sources/Thread/Thread.cpp.o"
   "CMakeFiles/bomberman.dir/Sources/Thread/ThreadPool.cpp.o"
   "CMakeFiles/bomberman.dir/Sources/main.cpp.o"
-  "bomberman"
   "bomberman.pdb"
+  "bomberman"
 )
 
 # Per-language clean rules from dependency scanning.
